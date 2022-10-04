d32d09034e6321c8697452c55a2fb9436bd4eaf0
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-10-04 21:40:27 +0300

    [add] - connection to backend, getEmployees method, bootstrap html template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from "./models/employee";
+import {EmployeeService} from "./services/employee.service";
+import {HttpErrorResponse} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'employee-manager-frontend';
+
+  public employees: Array<Employee> = [];
+
+  constructor(private  employeeService: EmployeeService) {}
+
+  ngOnInit(): void {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+
+
+
+
 }
