d2e6896bd980fb92d089d98c9e8015d79f461b32
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-10-05 14:41:58 +0300

    [add] - add Employee form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Employee} from "./models/employee";
 import {EmployeeService} from "./services/employee.service";
 import {HttpErrorResponse} from "@angular/common/http";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
@@ -31,8 +32,44 @@ export class AppComponent implements OnInit {
     );
   }
 
+  public onAddEmployee(addForm: NgForm): void {
+    // @ts-ignore
+    document.getElementById('add-employee-form').click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    )
+  }
+
+  public onOpenModal(_: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    if (mode === 'add') {
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    // if (mode === 'edit') {
+    //   this.editEmployee = employee;
+    //   button.setAttribute('data-target', '#updateEmployeeModal');
+    // }
+    // if (mode === 'delete') {
+    //   this.deleteEmployee = employee;
+    //   button.setAttribute('data-target', '#deleteEmployeeModal');
+    // }
+    // @ts-ignore
+    container.appendChild(button);
+    button.click();
+  }
 
 
 
 
-}
+    }
