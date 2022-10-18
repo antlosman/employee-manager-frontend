921f11fe201a925bcb75d8ee89a7c9572da92eaf
Anton Losman <87125898+antlosman@users.noreply.github.com>
2022-10-18 16:11:11 +0300

    [add] - delete, update and search Employee features added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,10 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'employee-manager-frontend';
 
   public employees: Array<Employee> = [];
+  public editEmployee!: Employee;
+  public deleteEmployee!: Employee;
 
   constructor(private  employeeService: EmployeeService) {}
 
@@ -33,11 +34,11 @@ export class AppComponent implements OnInit {
   }
 
   public onAddEmployee(addForm: NgForm): void {
-    // @ts-ignore
-    document.getElementById('add-employee-form').click();
+    document.getElementById('add-employee-form')!.click();
     this.employeeService.addEmployee(addForm.value).subscribe(
       (response: Employee) => {
         console.log(response);
+        this.getEmployees();
         addForm.reset();
       },
       (error: HttpErrorResponse) => {
@@ -47,7 +48,48 @@ export class AppComponent implements OnInit {
     )
   }
 
-  public onOpenModal(_: Employee, mode: string): void {
+  public onUpdateEmployee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  public onDeleteEmployee(employeeId: number): void {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message)
+      }
+    )
+  }
+
+  public searchEmployees(key: string): void {
+    console.log(key);
+    const results: Employee[] = [];
+    for (const employee of this.employees) {
+      if (employee.name.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.email.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.phone.toLowerCase().indexOf(key.toLowerCase()) !== -1
+        || employee.jobTitle.toLowerCase().indexOf(key.toLowerCase()) !== -1) {
+        results.push(employee);
+      }
+    }
+    this.employees = results;
+    if (results.length === 0 || !key) {
+      this.getEmployees();
+    }
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void {
     const container = document.getElementById('main-container');
     const button = document.createElement('button');
     button.type = 'button';
@@ -56,16 +98,15 @@ export class AppComponent implements OnInit {
     if (mode === 'add') {
       button.setAttribute('data-target', '#addEmployeeModal');
     }
-    // if (mode === 'edit') {
-    //   this.editEmployee = employee;
-    //   button.setAttribute('data-target', '#updateEmployeeModal');
-    // }
-    // if (mode === 'delete') {
-    //   this.deleteEmployee = employee;
-    //   button.setAttribute('data-target', '#deleteEmployeeModal');
-    // }
-    // @ts-ignore
-    container.appendChild(button);
+    if (mode === 'edit') {
+      this.editEmployee = employee;
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode === 'delete') {
+      this.deleteEmployee = employee;
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    container!.appendChild(button);
     button.click();
   }
 
